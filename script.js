05f4b75c32654cfa62ba3d81d59e4f39306aa9b6
Lucas Cerqueira <lucascerqueiraalves0@gmail.com>
2022-03-11 18:13:55 -0300

    Bateria de musica simples

---
+++ script.js
@@ -0,0 +1,53 @@
+//corpo do site
+document.body.addEventListener('keyup', (event)=>{
+    playSound(event.code.toLowerCase());
+});
+
+document.querySelector('.composer button').addEventListener('click', ()=>{
+    let song = document.querySelector('#input').value;
+    if (song !== '') {
+        let songArray = song.split('');
+        playComposition(songArray);
+    }
+});
+
+function playSound(sound) {
+    let audioElement = document.querySelector(`#s_${sound}`);
+    let keylement = document.querySelector(`div[data-key="${sound}"]`);
+    if(audioElement){
+        audioElement.currentTime = 0;
+        audioElement.play();
+    }
+
+    if(keylement){
+        keylement.classList.add('active');
+        
+
+        setTimeout(()=>{
+            keylement.classList.remove('active');
+        }, 100);
+    }
+}
+
+function playComposition(songArray) {
+    let wait = 0;
+
+    for(let songItem of songArray){
+
+        setTimeout(()=>{
+            playSound(`key${songItem}`);
+
+        }, wait);
+
+        wait += 250;
+       
+    }
+}
+
+
+
+
+
+
+
+
